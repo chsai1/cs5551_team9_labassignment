32dd4f3cb2455e79f15563fbdf548993c6edcf37
saidivyachennupati <42659823+saidivyachennupati@users.noreply.github.com>
2018-12-03 21:56:57 -0600

    updated

--- Lab 3/Source code/MongoConnection.js
+++
@@ -1,8 +0,0 @@
-var MongoClient = require('mongodb').MongoClient;
-var assert = require('assert');
-var url = 'mongodb://nikitha:nikki123@ds135993.mlab.com:35993/icp_9';
-MongoClient.connect(url, function(err, db) {
-    assert.equal(null, err);
-    console.log("Connected correctly to server.");
-    db.close();
-});
